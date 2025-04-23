95428fdf39caa130983a0e696003fc9c0d8da63e
Sean Daniel Celerio <sdcelerio@gmail.com>
2025-04-23 17:43:05 -0500

    Updated and printed new models for motor side and end side prints. Updated and tested basic opening and closing functions. Light sensor will be tested on its own before integration.

--- Automatic_Curtain.ino
+++ Automatic_Curtain.ino
@@ -1,12 +1,36 @@
+#include <Adafruit_VEML7700.h>
+
 #define PUL_PIN   2       // Alternate between High and Low to take single steps
 #define DIR_PIN   3       // Low is counter clockwise direction while High is clockwise
 #define ENA_PIN   4       // High enables stepper motor driver
-#define NUM_ROTATIONS 10  // Number of rotations the motor will perform in open/close sequences
+#define NUM_ROTATIONS 15  // Number of rotations the motor will perform in open/close sequences
 
 bool IsClosed = false;
 
-/* Open/Close Motor Function */
-void CycleMotor() {
+/* Open Motor Function */
+void OpenMotor() {
+  /* Update open/close state flag */
+  IsClosed = false;
+  digitalWrite(DIR_PIN, LOW);
+
+  /* Perform NUM_ROTATION rotations*/
+  for (unsigned int CurrRotation = 0; CurrRotation < NUM_ROTATIONS; CurrRotation++) {
+    /* Performs 200 steps, which is equal to one rotation */
+    for (unsigned int CurrStep = 0; CurrStep < 200; CurrStep++) {
+      digitalWrite(PUL_PIN, LOW);
+      delay(1);
+      digitalWrite(PUL_PIN, HIGH);
+      delay(1);
+    }
+  }
+}
+
+/* Close Motor Function */
+void CloseMotor() {
+  /* Update open/close state flag */
+  IsClosed = true;
+  digitalWrite(DIR_PIN, HIGH);
+
   /* Perform NUM_ROTATION rotations*/
   for (unsigned int CurrRotation = 0; CurrRotation < NUM_ROTATIONS; CurrRotation++) {
     /* Performs 200 steps, which is equal to one rotation */
@@ -32,6 +56,9 @@ void setup() {
 }
 
 void loop() {
-  CycleMotor();
+  CloseMotor();
+  delay(1000);
+
+  OpenMotor();
   delay(1000);
 }
