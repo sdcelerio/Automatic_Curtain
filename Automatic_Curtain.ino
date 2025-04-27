61ec05f2a467244c34bafe23b0842d7f921783b4
Sean Daniel Celerio <sdcelerio@gmail.com>
2025-04-27 00:48:33 -0500

    Added Initialization function and tested all VEML7700 code. All code works properly and all values that need changing is the light sensor polling period, the open lux threshold, and the close lux threshold.

--- Automatic_Curtain.ino
+++ Automatic_Curtain.ino
@@ -1,24 +1,37 @@
 #include <Wire.h>               // Arduino Library for I2C and other serial communication
-#include <Adafruit_VEML7700.h>  // Driver library for VEML7700 light sensor
+#include "Adafruit_VEML7700.h"  // Driver library for VEML7700 light sensor
 
-#define PUL_PIN   2                 // Alternate between High and Low to take single steps
+#define ENA_PIN   2                 // High enables stepper motor driver
 #define DIR_PIN   3                 // Low is counter clockwise direction while High is clockwise
-#define ENA_PIN   4                 // High enables stepper motor driver
-#define NUM_ROTATIONS 15            // Number of rotations the motor will perform in open/close sequences
-#define LIGHT_SENSOR_PERIOD_SEC 60  // Number of secinds that the Arudino will poll the VEML7700 light sensor
-#define LUX_OPEN_THRESHOLD 1000     // Lux level that will open the curtain when lux is above the given threshold
-#define LUX_CLOSE_THRESHOLD 700     // Lux level that will close the curtain when lux is below the given threshold
+#define PUL_PIN   4                 // Alternate between High and Low to take single steps
+#define NUM_ROTATIONS 18            // Number of rotations the motor will perform in open/close sequences
+#define LIGHT_SENSOR_PERIOD_SEC 1   // Number of secinds that the Arudino will poll the VEML7700 light sensor
+#define LUX_OPEN_THRESHOLD 500      // Lux level that will open the curtain when lux is above the given threshold
+#define LUX_CLOSE_THRESHOLD 200     // Lux level that will close the curtain when lux is below the given threshold
 
 /* Global variables/flags */
 Adafruit_VEML7700 veml = Adafruit_VEML7700(); // Object that represents the VEML7700 light sensor
-float lux = 0;
+float lux = 0;                                // Float indicating the light level 
 bool IsClosed = false;                        // Flag indicating open/close status
 
 /* Function declarations */
+void SetMotorZero();
 void OpenMotor();
 void CloseMotor();
 
 void setup() {
+  /* Initialize Serial BUS's */
+  Serial.begin(9600);
+  while (!Serial) delay(10);
+
+  /* Initialize VEML7700 Light sensor */
+  while (!veml.begin()) {
+    Serial.println("VEML7700 not found. Check connections.");
+    delay(1000);
+  }
+  veml.setGain(VEML7700_GAIN_1);
+  veml.setIntegrationTime(VEML7700_IT_100MS);
+
   /* Initialize all motor driver control pins */
   pinMode(PUL_PIN, OUTPUT);
   digitalWrite(PUL_PIN, LOW);  // Start Pulsing pin as Low
@@ -26,42 +39,56 @@ void setup() {
   digitalWrite(DIR_PIN, LOW); // Starting Clockwise Direction
   pinMode(ENA_PIN, OUTPUT);
   digitalWrite(ENA_PIN, HIGH); // Turns on Stepper Motor Driver
-
-  /* Initialize VEML7700 Light sensor */
-  Serial.begin(9600);
-  while (!Serial) delay(10);
-
-  if (!veml.begin()) {
-    Serial.println("VEML7700 not found. Check connections.");
-    while (1) delay(10);
-  }
-
-  veml.setGain(VEML7700_GAIN_1);
-  veml.setIntegrationTime(VEML7700_IT_100MS);
+  SetMotorZero();
 }
 
 void loop() {
   /* Read and update to current light level */
   lux = veml.readLux();
+  while (lux == NAN) {
+    Serial.println("VEML7700 not found. Check connections.");
+    delay(1000);
+    lux = veml.readLux();
+  }
   Serial.print(lux);
-
+  Serial.print("\n");
+  
   /* Perform motor operations if needed */
-  if (lux >= LUX_OPEN_THRESHOLD) 
-    Serial.print("OPENED");
+  if(lux >= LUX_OPEN_THRESHOLD)
+    OpenMotor();
   else if (lux <= LUX_CLOSE_THRESHOLD)
-    Serial.print("CLOSED");
+    CloseMotor();
 
   delay(LIGHT_SENSOR_PERIOD_SEC * 1000); // Wait for LIGHT_SENSOR_PERIOD_SEC seconds till next read 
 }
 
 /* Function Definitions */
+// Motor Initialization Function
+void SetMotorZero() {
+  IsClosed = false;
+  Serial.println("Setting the Curtain to open and setting starting position");
+  digitalWrite(DIR_PIN, LOW);
+  for (unsigned int CurrRotation = 0; CurrRotation < 20; CurrRotation++) {
+    /* Performs 200 steps, which is equal to one rotation */
+    for (unsigned int CurrStep = 0; CurrStep < 200; CurrStep++) {
+      digitalWrite(PUL_PIN, LOW);
+      delay(1);
+      digitalWrite(PUL_PIN, HIGH);
+      delay(1);
+    }
+  }
+}
+
 // Open Motor Function
 void OpenMotor() {
-  /* Update open/close state flag */
+  /* Check and update open/close state flag */
+  if (IsClosed == false) // Avoids opening the curtain twice
+    return;
   IsClosed = false;
-  digitalWrite(DIR_PIN, LOW);
-
+  
   /* Perform NUM_ROTATION rotations*/
+  Serial.println("Opening the Curtain");
+  digitalWrite(DIR_PIN, LOW);
   for (unsigned int CurrRotation = 0; CurrRotation < NUM_ROTATIONS; CurrRotation++) {
     /* Performs 200 steps, which is equal to one rotation */
     for (unsigned int CurrStep = 0; CurrStep < 200; CurrStep++) {
@@ -76,10 +103,13 @@ void OpenMotor() {
 // Close Motor Function
 void CloseMotor() {
   /* Update open/close state flag */
+  if (IsClosed == true) // Avoids closing  the curtain twice
+    return;
   IsClosed = true;
-  digitalWrite(DIR_PIN, HIGH);
 
   /* Perform NUM_ROTATION rotations*/
+  Serial.println("Closing the Curtain");
+  digitalWrite(DIR_PIN, HIGH);
   for (unsigned int CurrRotation = 0; CurrRotation < NUM_ROTATIONS; CurrRotation++) {
     /* Performs 200 steps, which is equal to one rotation */
     for (unsigned int CurrStep = 0; CurrStep < 200; CurrStep++) {
